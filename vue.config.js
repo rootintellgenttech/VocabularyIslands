e08c02a580fff88665535c77731fc13acf0cadbb
Margo Chin <68723858+margomin98@users.noreply.github.com>
2026-03-27 15:49:51 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,24 +4,14 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
-    host: '0.0.0.0', 
-    port: 443, 
-    
-    server: 'https', 
-    
-    allowedHosts: [
-      'englishability.rootadviser.com',
-      'all'
-    ],
-    client: {
-      overlay: false,
-    },
-    historyApiFallback: true,
+    allowedHosts: 'all', // 允許 ngrok 的域名訪問
   },
   css: {
     loaderOptions: {
       scss: {
-        additionalData: `@import "@/assets/scss/_utils.scss";`
+        additionalData: `
+          @import "@/assets/scss/_utils.scss";
+        `
       }
     }
   },
